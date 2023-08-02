f7b55291cf39ed1ad54230f2ad87cbeac68f71dc
essey1 <esseyt3@gmail.com>
2023-08-02 20:47:32 +0300

    minor changes

--- script.js
+++ script.js
@@ -12,6 +12,27 @@ navLinks.forEach(link => {
     })
 })
 
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
 // Contact me form
 const inputs = document.querySelectorAll(".input");
 function addFocus() {
